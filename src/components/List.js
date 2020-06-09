b092d12ea2595b85fde6c2939f4e1dbd127b8008
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2020-06-09 23:28:49 +0530

    RenderItem added

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { FlatList } from "react-native";
 
-const List = () => {
+const List = (props) => {
   return (
     <FlatList
       data={[1, 2, 3]}
       keyExtractor={(value, index) => index.toString()}
+      renderItem={(item, index) => {
+        <View>
+          <Text>{item}</Text>
+        </View>;
+      }}
     />
   );
 };
