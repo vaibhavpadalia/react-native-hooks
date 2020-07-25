33509b3162dd9e08d87e79575c713dbf99274749
Vaibhav padalia <vaibhav.padalia@kelltontech.com>
2020-07-25 23:15:03 +0530

    Changes in insult

--- src/components/Insults.js
+++ src/components/Insults.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useReducer } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { callRemoteMethod } from "../utilities/WebServiceHandler";
+import { URL } from "../utilities/Constants";
 
 const insultListReducer = (currentState, action) => {
   switch (action.type) {
@@ -12,6 +14,15 @@ const insultListReducer = (currentState, action) => {
 
 const Insults = ({ navigation }) => {
   const [insultList, dispatch] = useReducer(insultListReducer, []);
+
+  useEffect(() => {
+    callRemoteMethod(URL.INSULT_LIST, "GET", apiCallback);
+  }, []);
+
+  // Function that is called after Insult API is executed
+  const apiCallback = res => {
+    // setPersonDetails(res);
+  };  
 
   return (
     <View>
--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -2,6 +2,7 @@ module.exports = {
   URL: {
     GET_LOCATION: "http://ip-api.com/json/24.48.0.1",
     SWAPI_PEOPLE: "https://swapi.dev/api/people/1/",
-    PEOPLE_LIST: "https://swapi.dev/api/people/"
+    PEOPLE_LIST: "https://swapi.dev/api/people/",
+    INSULT_LIST: "https://evilinsult.com/generate_insult.php?lang=en&type=json"
   }
 }
