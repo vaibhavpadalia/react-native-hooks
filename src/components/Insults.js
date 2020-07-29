1f5b024482b97344b78157f664bcaeb7c8cb9914
Vaibhav padalia <vaibhav.padalia@kelltontech.com>
2020-07-29 23:08:44 +0530

    Implemented useReducer hook

--- src/components/Insults.js
+++ src/components/Insults.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useReducer } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from "react-native";
 import { callRemoteMethod } from "../utilities/WebServiceHandler";
 import { URL } from "../utilities/Constants";
 
 const insultListReducer = (currentState, action) => {
   switch (action.type) {
     case "SET":
-      return action.data;
+      return [action.data];
     case "ADD":
       return [...currentState, action.data];
   }
@@ -21,14 +21,37 @@ const Insults = ({ navigation }) => {
 
   // Function that is called after Insult API is executed
   const apiCallback = res => {
-    // setPersonDetails(res);
+    dispatch({ type: "SET", data: res.insult });
   };  
 
   return (
-    <View>
-      <Text>{"Insult component"}</Text>
-    </View>
+      <FlatList
+        style={styles.container}
+        data={insultList}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={styles.cardView}>
+              <Text>{`${index+1}: ${item}`}</Text>
+            </View>
+          );
+        }}
+      />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  peopleListText: {
+    margin: 10,
+    marginBottom: 5
+  },
+  cardView: {
+    margin: 10,
+    backgroundColor: "#F0F0F0"
+  },
+});
 
 export default Insults;
