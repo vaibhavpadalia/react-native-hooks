054e782d43a366533719accec91090f8e8ff6363
Vaibhav padalia <vaibhav.padalia@kelltontech.com>
2020-09-23 09:47:16 +0530

    Context added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Signin from './../components/Signin';
-import Signup from './../components/Signup';
-import Home from '../components/Home';
-import Insults from '../components/Insults';
+import Signin from "./../components/Signin";
+import Signup from "./../components/Signup";
+import Home from "../components/Home";
+import Insults from "../components/Insults";
 
 const AuthStack = createStackNavigator();
 
 const Routes = () => {
+  const [token, setToken] = useState(null);
+  const authContext = React.useMemo(() => {
+    return {
+      signIn: () => {
+        setToken("sampletoken");
+      },
+      signOut: () => {
+        setToken(null);
+      }
+    };
+  }, []);
+
   return (
     <NavigationContainer>
       <AuthStack.Navigator>
-        <AuthStack.Screen name={"Signin"} component={Signin} />
-        <AuthStack.Screen name={"Signup"} component={Signup} />
-        <AuthStack.Screen name={"Home"} component={Home} />
-        <AuthStack.Screen name={"Insults"} component={Insults} />
+        {token ? (
+          <>
+            <AuthStack.Screen name={"Signin"} component={Signin} />
+            <AuthStack.Screen name={"Signup"} component={Signup} />
+          </>
+        ) : (
+          <>
+            <AuthStack.Screen name={"Home"} component={Home} />
+            <AuthStack.Screen name={"Insults"} component={Insults} />
+          </>
+        )}
       </AuthStack.Navigator>
     </NavigationContainer>
   );
